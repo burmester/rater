753bc9ae9ee21f1a42eebf81b09df701dcf8fa84
Johan Burmester <johanbur@kth.se>
2019-05-17 09:25:20 +0200

    klart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [grades, setGrade] = useState([{ "grade": "F", "count": 0 }, { "grade": "E", "count": 0 }, { "grade": "C", "count": 0 }, { "grade": "A", "count": 0 }]);
+  const [showResult, setShowResult] = useState(false);
+
+  if (showResult) {
+    return (
+      <div className="App">
+        <div className="Flex">
+          {grades.map(grade => {
+            return (
+              <div key={grade.grade} className={"Smile " + grade.grade}>
+                  {grade.count}
+              </div>
+            )
+          })}
+        </div>
+        <div className="footer">
+          <button onClick={() => {
+            setShowResult(false)
+          }}>
+            Dölj resultat
+          </button>
+          <button onClick={() => {
+            setGrade([{ "grade": "F", "count": 0 }, { "grade": "E", "count": 0 }, { "grade": "C", "count": 0 }, { "grade": "A", "count": 0 }]);
+            setShowResult(false)
+          }}>
+            Nollställ
+          </button>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Flex">
+        {grades.map(grade => {
+          return (
+            <button className={"Smile " + grade.grade} key={grade.grade} onClick={() => {
+              const newGrades = [...grades];
+              newGrades.find(g => g.grade === grade.grade).count += 1;
+              setGrade(newGrades);
+            }}>
+            </button>
+          )
+        })}
+      </div>
+      <div className="footer">
+        <button onClick={() => {
+          setShowResult(true)
+        }}>Visa resultat</button>
+      </div>
     </div>
   );
 }
